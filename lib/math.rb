efc27f45e965ecfce21c74f2b06b7931a00d31f7
Dayne Dewar <daynedewar@yahoo.com>
2020-12-26 03:28:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  number = num1 - num2
+  puts number
 end
 
 def division(num1, num2)
-
+  number = num1 / num2
+  puts number
 end
 
 def multiplication(num1, num2)
-
+  number = num1 * num2
+  puts number
 end
 
 def modulo(num1, num2)
-
+  number = num1 % num2
+  puts number
 end
 
 def square_root(num)
-
+  
 end
